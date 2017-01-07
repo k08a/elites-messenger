d93f5e14e6f9dec4c6787709590ff78da584f3e1
k08a <etnallirb.a@gmail.com>
2017-01-07 07:18:17 +0000

    07_実習終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   root to: 'timelines#index'
+  
+    devise_for :users  
 
-  devise_for :users
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+  
+
+  
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
